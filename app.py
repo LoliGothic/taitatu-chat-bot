dc7219cacd6229fd0fd4dc99aabc531117926e11
daisukemiyoshi <nana40481023@gmail.com>
2022-08-03 19:01:44 +0900

    Fix apiキー

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
+import os
 import gspread
 import json
 import random
@@ -20,9 +21,14 @@ from oauth2client.service_account import ServiceAccountCredentials
 #2つのAPIを記述しないとリフレッシュトークンを3600秒毎に発行し続けなければならない
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 
+key = os.environ['apiKey']
+
+with open("secret.json", "w") as outputFile:
+    json.dump(key, outputFile, indent=2 )
+
 #認証情報設定
 #ダウンロードしたjsonファイル名をクレデンシャル変数に設定（秘密鍵、Pythonファイルから読み込みしやすい位置に置く）
-credentials = ServiceAccountCredentials.from_json_keyfile_name('gspread-338817-e4487b66d7b0.json', scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name('secret.json', scope)
 
 #OAuth2の資格情報を使用してGoogle APIにログイン
 gs = gspread.authorize(credentials)
