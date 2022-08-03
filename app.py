a09fbc03a2da1697d104d28087369043ee61a732
daisukemiyoshi <nana40481023@gmail.com>
2022-08-03 18:39:05 +0900

    Fix apiキー

--- app.py
+++ app.py
@@ -23,7 +23,8 @@ scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/aut
 
 #認証情報設定
 #ダウンロードしたjsonファイル名をクレデンシャル変数に設定（秘密鍵、Pythonファイルから読み込みしやすい位置に置く）
-credentials = ServiceAccountCredentials.from_json_keyfile_name(os.environ['apiKey'], scope)
+secretKey = os.environ['apiKey']
+credentials = ServiceAccountCredentials.from_json_keyfile_name(secretKey, scope)
 
 #OAuth2の資格情報を使用してGoogle APIにログイン
 gs = gspread.authorize(credentials)
