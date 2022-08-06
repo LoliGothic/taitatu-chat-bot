f7e389abe023d1af48749a406564e33dfa9425f8
daisukemiyoshi <nana40481023@gmail.com>
2022-08-06 22:52:52 +0900

    Fix apiキー設定

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, abort
-from boto.s3.connection import S3Connection
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -22,18 +21,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 #2つのAPIを記述しないとリフレッシュトークンを3600秒毎に発行し続けなければならない
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 
-credential = {
-                "type": "service_account",
-                "project_id": os.environ['SHEET_PROJECT_ID'],
-                "private_key_id": os.environ['SHEET_PRIVATE_KEY_ID'],
-                "private_key": os.environ['SHEET_PRIVATE_KEY'],
-                "client_email": os.environ['SHEET_CLIENT_EMAIL'],
-                "client_id": os.environ['SHEET_CLIENT_ID'],
-                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-                "token_uri": "https://oauth2.googleapis.com/token",
-                "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-                "client_x509_cert_url": os.environ['SHEET_CLIENT_X509_CERT_URL']
-             }
+credential = json.loads(os.environ["SHEET_KEY"])
 
 #認証情報設定
 #ダウンロードしたjsonファイル名をクレデンシャル変数に設定（秘密鍵、Pythonファイルから読み込みしやすい位置に置く）
